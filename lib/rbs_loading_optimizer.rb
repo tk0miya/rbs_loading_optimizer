461aeda00f1d71343d02502bf07c27a54b734ef2
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-06-07 02:11:21 +0900

    Initial commit (Just after "bundle gem rbs_loading_optimizer")

---
+++ lib/rbs_loading_optimizer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbs_loading_optimizer/version"
+
+module RbsLoadingOptimizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbs_loading_optimizer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RbsLoadingOptimizer
+  VERSION = "0.1.0"
+end
